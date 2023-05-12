7e97b77884a25d47016ace24009fc434b3646044
jmcarcell <jmcarcell@users.noreply.github.com>
2023-05-12 09:52:53 +0200

    Sorte the list of plots

--- scripts/plots/add_plots.py
+++ scripts/plots/add_plots.py
@@ -1,6 +1,6 @@
 import glob
 # Add all plots from a folder called plots to the LaTeX template
 with open('template.tex', 'a') as f:
-    for p in glob.glob('plots/*.pdf'):
+    for p in sorted(glob.glob('plots/*.pdf')):
         f.write(fr'\includegraphics[width=\textwidth]{{{p}}}' + '\n')
     f.write(r'\end{document}')
