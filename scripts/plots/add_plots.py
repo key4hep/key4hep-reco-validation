3d726a5d96376ab159ee8fb2622f7040a04bc68c
jmcarcell <jmcarcell@users.noreply.github.com>
2023-05-12 09:50:57 +0200

    Add a newline after each plot

--- scripts/plots/add_plots.py
+++ scripts/plots/add_plots.py
@@ -1,6 +1,6 @@
 import glob
 # Add all plots from a folder called plots to the LaTeX template
-with open('template.tex', 'ra') as f:
+with open('template.tex', 'a') as f:
     for p in glob.glob('plots/*.pdf'):
-        f.write(r'\includegraphics[width=\textwidth]{' + p + '}')
+        f.write(fr'\includegraphics[width=\textwidth]{{{p}}}' + '\n')
     f.write(r'\end{document}')
