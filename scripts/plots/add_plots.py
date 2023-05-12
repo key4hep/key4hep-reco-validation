e14b9a7ba5193ca5a0922f034f857c5f9cad4ce7
jmcarcell <jmcarcell@users.noreply.github.com>
2023-05-12 10:17:38 +0200

    Put comment at the top

--- scripts/plots/add_plots.py
+++ scripts/plots/add_plots.py
@@ -1,5 +1,5 @@
-import glob
 # Add all plots from a folder called plots to the LaTeX template
+import glob
 with open('template.tex', 'a') as f:
     for p in sorted(glob.glob('plots/*.pdf')):
         f.write(fr'\includegraphics[width=\textwidth]{{{p}}}' + '\n')
