7a90749b67bc741f68d05f10bbdbad854afc5297
jmcarcell <jmcarcell@users.noreply.github.com>
2023-06-01 10:07:46 +0200

    Update with the new improvements

--- scripts/make_jet_plots.py
+++ scripts/make_jet_plots.py
@@ -9,22 +9,22 @@ label_map = {'recoJetE': 'Reconstructed Jet Energy',
              'recoJetPx': 'Reconstructed Jet $P_x$',
              'recoJetPy': 'Reconstructed Jet $P_y$',
              'recoJetPz': 'Reconstructed Jet $P_z$',
-             'genJetE': 'Generated Jet Energy',
-             'genJetPx': 'Generated Jet $P_x$',
-             'genJetPy': 'Generated Jet $P_y$',
-             'genJetPz': 'Generated Jet $P_z$',
+             'genJetE': 'MC Jet Energy',
+             'genJetPx': 'MC Jet $P_x$',
+             'genJetPy': 'MC Jet $P_y$',
+             'genJetPz': 'MC Jet $P_z$',
              'E_totPFO': 'Total PFO Energy',
-             'E_trueInv': 'True Invisible Energy',
-             'E_trueVis': 'True Visible Energy',
+             'E_trueInv': 'MC Invisible Energy',
+             'E_trueVis': 'MC Visible Energy',
              'Px_totPFO': 'Total PFO $P_x$',
              'Py_totPFO': 'Total PFO $P_y$',
              'Pz_totPFO': 'Total PFO $P_z$',
-             'Px_trueInv': 'True Invisible $P_x$',
-             'Py_trueInv': 'True Invisible $P_y$',
-             'Pz_trueInv': 'True Invisible $P_z$',
-             'Px_trueVis': 'True Visible $P_x$',
-             'Py_trueVis': 'True Visible $P_y$',
-             'Pz_trueVis': 'True Visible $P_z$',
+             'Px_trueInv': 'MC Invisible $P_x$',
+             'Py_trueInv': 'MC Invisible $P_y$',
+             'Pz_trueInv': 'MC Invisible $P_z$',
+             'Px_trueVis': 'MC Visible $P_x$',
+             'Py_trueVis': 'MC Visible $P_y$',
+             'Pz_trueVis': 'MC Visible $P_z$',
              'd1_mcE': 'Daughter 1 MC Energy',
              'd2_mcE': 'Daughter 2 MC Energy',
              'd1_mcPDGID': 'Daughter 1 MC PDGID',
@@ -42,6 +42,9 @@ label_map = {'recoJetE': 'Reconstructed Jet Energy',
 # Number of sigmas away from the mean to make the plot red
 threshold = 3
 
+# x, y, width, height
+table_dimensions_left = [0.5, 0.7, 0.25, 0.2]
+table_dimensions_right = [0.75, 0.7, 0.25, 0.2]
 
 def make_jet_plots(root_file, reference_root_file):
     upr = uproot.open(root_file)['showerData;1']
@@ -58,16 +61,22 @@ def make_jet_plots(root_file, reference_root_file):
         else:
             dist_current = np.concatenate(arrays[0][k])
             dist_reference = np.concatenate(arrays[1][k])
-        _, bins, _ = ax.hist(dist_current, bins=15, histtype='step', label='Current')
-        ax.hist(dist_reference, bins=bins, histtype='step', label='Reference')
+        if 'pdg' in k.lower():
+            bins = 'auto'
+        elif k == 'E_trueInv':
+            bins = [-.5, .5]
+        else:
+            bins = np.linspace(min(dist_current.min(), dist_reference.min()), max(dist_current.max(), dist_reference.max()), 15)
+        _, bins, _ = ax.hist(dist_current, bins=bins, histtype='step', label='Current', density=True)
+        ax.hist(dist_reference, bins=bins, histtype='step', label='Reference', density=True)
 
         left = ['Mean', 'Std. Dev.', 'Entries']
         right_current_values = [dist_current.mean(), dist_current.std(), len(dist_current)]
         right_current_labels = [f'{right_current_values[0]:.2f}', f'{right_current_values[1]:.2f}', f'{right_current_values[2]}']
-        add_table(left, right_current_labels, ax, [0.6, 0.4, 0.2, 0.2])
+        add_table(left, right_current_labels, ax, table_dimensions_left, title='Current')
         right_reference_values = [dist_reference.mean(), dist_reference.std(), len(dist_reference)]
         right_reference_labels = [f'{right_reference_values[0]:.2f}', f'{right_reference_values[1]:.2f}', f'{right_reference_values[2]}']
-        add_table(left, right_reference_labels, ax, [0.8, 0.4, 0.2, 0.2])
+        add_table(left, right_reference_labels, ax, table_dimensions_right, title='Reference')
 
         # If the mean is more than threshold sigma away from the reference, make the plot red
         if abs(right_current_values[0] - right_reference_values[0]) > threshold*(right_reference_values[1] / np.sqrt(len(dist_reference))):
@@ -76,17 +85,37 @@ def make_jet_plots(root_file, reference_root_file):
         ax.legend()
         ax.set_ylabel('Entries')
         ax.set_xlabel(label_map[k])
+        ax.set_ylim(ax.get_ylim()[0], ax.get_ylim()[1]*1.2)
         fig.savefig(f'jet-{k}.png')
         ax.clear()
+        fig.patch.set_facecolor('white')
+
+        if k == 'genJetE':
+            print(dist_current, dist_reference)
+            print(right_current_values, right_reference_values)
+            print(abs(right_current_values[0] - right_reference_values[0]), threshold*(right_reference_values[1] / np.sqrt(len(dist_reference))))
 
     for gen, reco in [['genJetE', 'recoJetE'], ['genJetPx', 'recoJetPx'], ['genJetPy', 'recoJetPy'], ['genJetPz', 'recoJetPz']]:
-        _, bins, _ = ax.hist(np.concatenate(arrays[0][gen])-np.concatenate(arrays[0][reco]),
-                             bins=20, histtype='step', label='Current')
-        ax.hist(np.concatenate(arrays[1][gen])-np.concatenate(arrays[1][reco]),
-                bins=bins, histtype='step', label='Reference')
+        dist_current = np.concatenate(arrays[0][gen])-np.concatenate(arrays[0][reco])
+        dist_reference = np.concatenate(arrays[1][gen])-np.concatenate(arrays[1][reco])
+        _, bins, _ = ax.hist(dist_current, bins=20, histtype='step', label='Current', density=True)
+        ax.hist(dist_reference, bins=bins, histtype='step', label='Reference', density=True)
+        left = ['Mean', 'Std. Dev.', 'Entries']
+        right_current_values = [dist_current.mean(), dist_current.std(), len(dist_current)]
+        right_current_labels = [f'{right_current_values[0]:.2f}', f'{right_current_values[1]:.2f}', f'{right_current_values[2]}']
+        add_table(left, right_current_labels, ax, table_dimensions_left, title='Current')
+        right_reference_values = [dist_reference.mean(), dist_reference.std(), len(dist_reference)]
+        right_reference_labels = [f'{right_reference_values[0]:.2f}', f'{right_reference_values[1]:.2f}', f'{right_reference_values[2]}']
+        add_table(left, right_reference_labels, ax, table_dimensions_right, title='Reference')
+
+        # If the mean is more than threshold sigma away from the reference, make the plot red
+        if abs(right_current_values[0] - right_reference_values[0]) > threshold*(right_reference_values[1] / np.sqrt(len(dist_reference))):
+            fig.patch.set_facecolor('red')
+
         ax.legend()
         ax.set_xlabel(f'{label_map[gen]} - {label_map[reco]}')
         ax.set_ylabel('Entries')
+        ax.set_ylim(ax.get_ylim()[0], ax.get_ylim()[1]*1.2)
         fig.savefig(f'jet-{gen}-{reco}.png')
         ax.clear()
 
