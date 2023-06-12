76e2e4459ceb91836197f7face68b68895c5048b
jmcarcell <jmcarcell@users.noreply.github.com>
2023-06-12 20:25:36 +0200

    Try to fix the plot runner

--- scripts/plot_runner.py
+++ scripts/plot_runner.py
@@ -26,7 +26,7 @@ for run in to_run:
         run['function'](filenames[run['name']], os.path.join(args.reference, filenames[run['name']]))
         # Move all png files to the corresponding folder
         os.makedirs(os.path.join(args.output, run['name'], 'plots'), exist_ok=True)
-        subprocess.run(['mv', '*.png', os.path.join(args.output, run['name'], 'plots')])
+        subprocess.run(['mv', '*.png', os.path.join(args.output, run['name'], 'plots')], shell=True)
         
     except Exception as e:
         print('Failed to run', run['name'], e)
