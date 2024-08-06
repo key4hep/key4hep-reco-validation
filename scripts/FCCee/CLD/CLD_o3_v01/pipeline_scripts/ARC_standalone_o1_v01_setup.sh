29a15228cbad7f4e2dec74eba680b098341f474c
Enrico Lupi <enrico.lupi@cern.ch>
2024-08-06 13:57:13 +0200

    DELETE useless git clone

--- scripts/FCCee/CLD/CLD_o3_v01/pipeline_scripts/ARC_standalone_o1_v01_setup.sh
+++ scripts/FCCee/CLD/CLD_o3_v01/pipeline_scripts/ARC_standalone_o1_v01_setup.sh
@@ -2,7 +2,6 @@
 
 echo "Downloading necessary Github repos..."
 
-git clone --depth 1 https://github.com/key4hep/k4geo.git
 git clone https://github.com/key4hep/CLDConfig.git
 
 echo "Download terminated - setup stage successful"
