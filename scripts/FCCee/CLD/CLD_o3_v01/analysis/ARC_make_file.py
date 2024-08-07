53d94e351d1a3bd0fe2568e11c450dac21583d9c
Enrico Lupi <enrico.lupi@cern.ch>
2024-08-07 19:13:50 +0200

    ADD use ROOT files for histograms and comparison

---
+++ scripts/FCCee/CLD/CLD_o3_v01/analysis/ARC_make_file.py
@@ -0,0 +1,90 @@
+import ROOT
+from podio import root_io
+import dd4hep as dd4hepModule
+from ROOT import dd4hep
+import argparse
+import numpy as np
+import os
+from dd4hep import Detector
+import DDRec
+
+
+def make_photon_file(args):
+  
+  # create output ROOT file
+  outputFile = ROOT.TFile(args.outputFile, "RECREATE")
+
+  # set file reader
+  podio_reader = root_io.Reader(args.inputFile)
+
+  # get detector description for cell id decoding
+  theDetector = Detector.getInstance()
+  theDetector.fromXML(os.environ["K4GEO"]+"/FCCee/CLD/compact/CLD_o3_v01/ARC_o1_v01.xml")
+  idposConv = DDRec.CellIDPositionConverter(theDetector)
+                  
+
+  ########## Count Photon Hits #########################
+
+  hist_nPh = ROOT.TH1F("h_nPh", "Total number of photon counts per event",
+                       50, 0, 250)
+  hist_theta = ROOT.TH1F("h_theta", "Number of photons counts per event vs. theta",
+                         90, 0, np.pi)
+  hist_theta_1stHit = ROOT.TH1F("h_theta_1stHit", "Number of photons counts per event vs. theta of incoming particle",
+                                90, 0, np.pi)
+  
+  # loop over dataset
+  for event in podio_reader.get("events"):
+
+    n_ph = 0
+
+    p = (event.get("MCParticles"))[0]
+    mom = ROOT.TVector3(p.getMomentum().x, p.getMomentum().y, p.getMomentum().z)
+    theta_1stHit = mom.Theta()
+
+    for arc_hit in event.get("ArcCollection"):
+      particle =  arc_hit.getMCParticle()
+      pdgID = particle.getPDG()
+      if pdgID == 22 or pdgID == -22:
+        n_ph += 1
+        cellID = arc_hit.getCellID()
+        x = idposConv.position(cellID) 
+        theta = x.theta()
+
+        hist_theta.Fill(theta)
+
+    hist_nPh.Fill(n_ph)
+    hist_theta_1stHit.Fill(theta_1stHit, n_ph)
+
+  hist_nPh.Write()
+  hist_theta.Write()
+  hist_theta_1stHit.Write()
+
+  # check for photon presence
+  photon_presence = True
+  if hist_theta.GetEntries() == 0:
+    photon_presence = False
+  
+  outputFile.Close()
+
+  return photon_presence
+  
+
+#########################################################################
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser(
+        description="Process simulation"
+    )
+  parser.add_argument('-f', "--inputFile",  type=str, 
+                      help='The name of the simulation file to be processed', default='ARC_sim.root')
+  parser.add_argument('-o', "--outputFile", type=str, 
+                      help='The name of the ROOT file where to save output histograms', default='ARC_analysis.root')
+  args = parser.parse_args()
+    
+  ph_presence = make_photon_file(args)
+  print(ph_presence)  
+  
+  
+  
+  
+  
--- scripts/FCCee/CLD/CLD_o3_v01/analysis/ARC_val_plots.py
+++ scripts/FCCee/CLD/CLD_o3_v01/analysis/ARC_val_plots.py
@@ -1,16 +1,34 @@
 import ROOT
-from podio import root_io
-import dd4hep as dd4hepModule
-from ROOT import dd4hep
+import uproot
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator, FuncFormatter
 from matplotlib.colors import SymLogNorm
 
 import argparse
 import numpy as np
+import sys
+import importlib.util
+from pathlib import Path
 import os
-from dd4hep import Detector
-import DDRec
+
+
+plot_params = {
+
+  "nPh" : {
+    "range" : (0, 250),
+    "xlabel": 'Number of Photons',
+    "ylabel": 'Photon count / 5',
+    "xticks": False,
+  },
+
+  "theta" : {
+    "range" : (0, np.pi),
+    "xlabel": r'Polar angle $\theta$',
+    "ylabel": f'Photon count / {np.pi/90:.2f} rad',
+    "xticks": True
+  }
+
+}
 
 
 def format_func(value, tick_number):
@@ -45,186 +63,67 @@ def format_func(value, tick_number):
       return r"-$\pi/6$"
     else:
       return r"${0}\pi/6$".format(N)
-    
-
-def count_photons(inputFile):
-
-  # set file reader
-  podio_reader = root_io.Reader(inputFile)
-
-  # get detector description for cell id decoding
-  theDetector = Detector.getInstance()
-  theDetector.fromXML(os.environ["K4GEO"]+"/FCCee/CLD/compact/CLD_o3_v01/CLD_o3_v01.xml")
-  idposConv = DDRec.CellIDPositionConverter(theDetector)
-  n_evts = len(podio_reader.get("events"))
-                  
-
-  ########## Count Photon Hits #########################
-  
-  # prepare arrays for quantities of interest
-  n_ph = np.zeros(n_evts)
-  first_hit = np.zeros((2, n_evts))
-  ph_theta = []
-  ph_phi   = []
-  
-  # loop over dataset
-  for i_evt, event in enumerate(podio_reader.get("events")):
-
-    p = (event.get("MCParticles"))[0]
-    mom = ROOT.TVector3(p.getMomentum().x, p.getMomentum().y, p.getMomentum().z)
-    first_hit[0,i_evt] = mom.Theta()
-    first_hit[1,i_evt] = mom.Phi()
-
-    for i_hit, arc_hit in enumerate(event.get("ArcCollection")):
-      particle =  arc_hit.getMCParticle()
-      pdgID = particle.getPDG()
-      if pdgID == 22 or pdgID == -22:
-        n_ph[i_evt] += 1
-        cellID = arc_hit.getCellID()
-        x = idposConv.position(cellID) 
-        ph_theta.append(x.theta())
-        ph_phi.append(x.phi())
-
-  return n_ph, first_hit, [ph_theta, ph_phi]
   
 
-def plot_ph_count(n_ph, first_hit, ph_count, args):
-
-  n_evt = len(n_ph)
-  print("Number of evts:", n_evt)
+def make_plots(args):
 
   filename = os.path.splitext(os.path.basename(args.inputFile))[0] # removes path and extension
   if args.outputPath[-1] != '/':
     args.outputPath += '/'
   output_start = args.outputPath + filename 
 
+  # read input file
+  inputFile = uproot.open(args.inputFile)
 
-  # photon count vs. theta
-
-  fig1, ax1 = plt.subplots(figsize=(8,6))
-  nbins=90
-  hist, bin_edges = np.histogram(ph_count[0], bins=nbins, range=(0, np.pi))
-  if not args.no_norm:
-    hist = hist.astype(float)/n_evt
-  ax1.bar(bin_edges[:-1], hist, width=np.diff(bin_edges),
-          color='lightblue', edgecolor='black', align='edge')
-  ax1.set_xlabel(r'Polar angle $\theta$')
-  ax1.set_ylabel(f'Photon count / {np.pi/nbins:.2f} rad')
-  ax1.set_yscale('log')
-  ax1.xaxis.set_major_locator(MultipleLocator(np.pi / 6))
-  ax1.xaxis.set_major_formatter(FuncFormatter(format_func))
-  
-  if not args.no_save:
-    fig1.savefig(output_start+'_phCount_theta.svg', bbox_inches='tight')
-  if args.no_show:
-    plt.close(fig1)
+  # check reference
+  if len(args.referenceFile) != 0:
+    refFile = uproot.open(args.referenceFile)
+    compare_module = importlib.import_module("compare_histos")
+    histograms_match = compare_module.compare_histos(args.inputFile, args.referenceFile, args.SL)
 
 
-  # photon count vs. theta and phi
+  # Plot histograms
+  for key in inputFile:
+    histo = inputFile[key]
+    edges = histo.axis().edges()
+    values = histo.values()
 
-  fig2, ax2 = plt.subplots(figsize=(8,6))
-  nbins_theta = 90
-  nbins_phi = 36
-  hist, x_edges, y_edges = np.histogram2d(ph_count[0], ph_count[1],
-                                          bins=[nbins_theta, nbins_phi], 
-                                          range=((0, np.pi), (-np.pi, np.pi)))
-  vmax=1e4
-  lim=1
-  if not args.no_norm:
-    hist = hist.astype(float)/n_evt
-    vmax/=n_evt
-    lim/=n_evt
-  img = plt.imshow(hist.T, origin='lower', extent=[x_edges[0], x_edges[-1], y_edges[0], y_edges[-1]],
-                   aspect='auto', cmap='viridis', norm=SymLogNorm(vmin=0, vmax=vmax, linthresh=lim))
-  cbar = plt.colorbar(img, ax=ax2)
-  cbar.set_label(rf'Counts / {np.pi/nbins_theta:.2f} rad ($\theta$) / {2*np.pi/nbins_phi:.2f} rad ($\phi$) ')
-  
-  ax2.set_xlabel(r'Polar angle $\theta$')
-  ax2.set_ylabel(r'Azimuthal angle $\phi$')
-  ax2.set_title('Photon Count')
-  ax2.xaxis.set_major_locator(MultipleLocator(np.pi / 6))
-  ax2.xaxis.set_major_formatter(FuncFormatter(format_func))
-  ax2.yaxis.set_major_locator(MultipleLocator(np.pi / 6))
-  ax2.yaxis.set_major_formatter(FuncFormatter(format_func))
+    param = {}
+    for p in plot_params:
+      if p in key:
+        param = plot_params[p]
+        break
+    if len(param) == 0:
+      print(f"No plot parameters found for histogram {key}!")
+      continue
 
-  if not args.no_save:
-    fig2.savefig(output_start+'_phCount_thetaphi.svg', bbox_inches='tight')
-  if args.no_show:
-    plt.close(fig2)
+    fig, ax = plt.subplots(figsize=(8,6))
+    ax.hist(edges[:-1], bins=edges, weights=values, histtype='step',
+             label='New hist', color='blue')
+    ax.set_xlabel(param['xlabel'])
+    ax.set_ylabel(param['ylabel'])
+    ax.set_title(f'{histo.title}')
+    ax.set_yscale('log')
+    if param['xticks']:
+      ax.xaxis.set_major_locator(MultipleLocator(np.pi / 6))
+      ax.xaxis.set_major_formatter(FuncFormatter(format_func))
 
-  
-  # number of photons per evt
+    if len(args.referenceFile) != 0:
+      ref_histo = refFile[key]
+      ref_edges = ref_histo.axis().edges()
+      ref_values = ref_histo.values()
+      ax.hist(ref_edges[:-1], bins=ref_edges, weights=ref_values, histtype='step',
+              label='New hist', color='red')
+      ax.legend(loc='best')
+      if not histograms_match[key.split(';')[0]]:
+        fig.patch.set_facecolor('red') 
 
-  fig3, ax3 = plt.subplots(figsize=(8,6))
-  nbins=50
-  ax3.hist(n_ph, bins=nbins, range=(0, 250),
-           color='lightblue', edgecolor='black')
-  ax3.set_xlabel('Number of photons')
-  ax3.set_ylabel(f'Photon count / 5')
-  ax3.set_yscale('log')
-  
-  if not args.no_save:
-    fig3.savefig(output_start+'_phCount_Nph.svg', bbox_inches='tight')
-  if args.no_show:
-    plt.close(fig3)
+    if not args.no_save:
+      fig.savefig(output_start+f'_{histo.name}'+'.svg', bbox_inches='tight')
+    if not args.show:
+      plt.close(fig)
 
-  
-  # photon count vs. theta of first hit
-
-  fig4, ax4 = plt.subplots(figsize=(8,6))
-  nbins=90
-  hist, bin_edges = np.histogram(first_hit[0], bins=nbins, range=(0, np.pi), weights=n_ph)
-  if not args.no_norm:
-    hist = hist.astype(float)/n_evt
-  ax4.bar(bin_edges[:-1], hist, width=np.diff(bin_edges),
-          color='lightblue', edgecolor='black', align='edge')
-  ax4.set_xlabel(r'Polar angle $\theta$ of primary particle')
-  ax4.set_ylabel(f'Photon count / {np.pi/nbins:.2f} rad')
-  ax4.set_yscale('log')
-  ax4.xaxis.set_major_locator(MultipleLocator(np.pi / 6))
-  ax4.xaxis.set_major_formatter(FuncFormatter(format_func))
-  
-  if not args.no_save:
-    fig4.savefig(output_start+'_phCount_theta_1stHit.svg', bbox_inches='tight')
-  if args.no_show:
-    plt.close(fig4)
-
-
-  # photon count vs. theta and phi of first hit
-
-  fig5, ax5 = plt.subplots(figsize=(8,6))
-  nbins_theta = 90
-  nbins_phi = 36
-  hist, x_edges, y_edges = np.histogram2d(first_hit[0], first_hit[1],
-                                          bins=[nbins_theta, nbins_phi], 
-                                          range=((0, np.pi), (-np.pi, np.pi)),
-                                          weights=n_ph)
-  vmax=1e4
-  lim=1
-  if not args.no_norm:
-    hist = hist.astype(float)/n_evt
-    vmax/=n_evt
-    lim/=n_evt
-  img = plt.imshow(hist.T, origin='lower', extent=[x_edges[0], x_edges[-1], y_edges[0], y_edges[-1]],
-                   aspect='auto', cmap='viridis', norm=SymLogNorm(vmin=0, vmax=vmax, linthresh=lim))
-  cbar = plt.colorbar(img, ax=ax5)
-  cbar.set_label(rf'Counts / {np.pi/nbins_theta:.2f} rad ($\theta$) / {2*np.pi/nbins_phi:.2f} rad ($\phi$) ')
-  
-  ax5.set_xlabel(r'Polar angle $\theta$ of primary particle')
-  ax5.set_ylabel(r'Azimuthal angle $\phi$ of primary particle')
-  ax5.set_title('Photon Count')
-  ax5.xaxis.set_major_locator(MultipleLocator(np.pi / 6))
-  ax5.xaxis.set_major_formatter(FuncFormatter(format_func))
-  ax5.yaxis.set_major_locator(MultipleLocator(np.pi / 6))
-  ax5.yaxis.set_major_formatter(FuncFormatter(format_func))
-
-  if not args.no_save:
-    fig5.savefig(output_start+'_phCount_thetaphi_1stHit.svg', bbox_inches='tight')
-  if args.no_show:
-    plt.close(fig5)
-
-
-  else:
+  if args.show:
     plt.show()
   
 
@@ -235,23 +134,20 @@ if __name__ == "__main__":
         description="Process simulation"
     )
   parser.add_argument('-f', "--inputFile",  type=str, 
-                      help='The name of the simulation file to be processed', default='arc_pi+_50GeV.root')
+                      help='The name of the simulation file to be processed', default='ARC_analysis.root')
   parser.add_argument('-o', "--outputPath", type=str, 
                       help='The name of the directory where to save output files', default='./')
+  parser.add_argument('-r', "--referenceFile", type=str, 
+                      help='The name of the file containing reference histos', default='')
   parser.add_argument("--no_save", action='store_true', help='Do not save output arrays')
-  parser.add_argument("--no_show", action='store_true', help='Do not plot output histograms')
-  parser.add_argument("--no_norm", action='store_true', help='Do not normalize output histograms by number of events')
+  parser.add_argument("--SL", type=float, default=0.95, 
+                      help='Significance level of the test to perform')
+  parser.add_argument("--show",    action='store_true', help='Plot output histograms')
+  #parser.add_argument("--no_norm", action='store_true', help='Do not normalize output histograms by number of events')
   
   args = parser.parse_args()
     
-  n_ph, first_hit, ph_count = count_photons(args.inputFile)
-  ph_presence = (len(ph_count[0]) != 0)
-  
-  if not(args.no_save and args.no_show):
-    if ph_presence:
-      plot_ph_count(n_ph, first_hit, ph_count, args)
-  
-  print(ph_presence)  
+  make_plots(args)
   
   
   
