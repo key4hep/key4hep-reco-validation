2541ed551a28ad479e7d2f7ab47264f2f58200a2
Juraj Smiesko <juraj.smiesko@cern.ch>
2025-06-04 09:17:46 +0200

    Running ALLEGRO in subshell

--- scripts/FCCee/ALLEGRO/ALLEGRO_o1_v03/ALLEGRO_o1_v03_script.sh
+++ scripts/FCCee/ALLEGRO/ALLEGRO_o1_v03/ALLEGRO_o1_v03_script.sh
@@ -4,23 +4,20 @@ set -e
 # setup phase
 echo "SETUP PHASE:"
 
-[ -z "$KEY4HEP_STACK" ] && source /cvmfs/sw-nightlies.hsf.org/key4hep/setup.sh
+[ -z "${KEY4HEP_STACK}" ] && source /cvmfs/sw-nightlies.hsf.org/key4hep/setup.sh
 
 
 # simulation phase
 echo "SIM-DIGI-RECO PHASE:"
 
 echo "Starting script..."
-source $FCCCONFIG/share/FCC-config/FullSim/ALLEGRO/ALLEGRO_o1_v03/ctest_sim_digi_reco.sh
+bash "${FCCCONFIG}/share/FCC-config/FullSim/ALLEGRO/ALLEGRO_o1_v03/ctest_sim_digi_reco.sh"
 
 
 # analyze simulation file
 echo "ANALYSIS PHASE:"
 
 echo "Starting analysis script..."
-python $WORKAREA/key4hep-reco-validation/scripts/FCCee/ALLEGRO/ALLEGRO_o1_v03/ALLEGRO_make_TH1.py \
+python "${WORKAREA}/key4hep-reco-validation/scripts/FCCee/ALLEGRO/ALLEGRO_o1_v03/ALLEGRO_make_TH1.py" \
        -f ALLEGRO_sim_digi_reco.root -o results.root
 echo "Script executed successfully"
-
-
-
