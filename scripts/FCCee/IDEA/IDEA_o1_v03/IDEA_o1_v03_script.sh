f0d9ffd03f747a4caa9eeec42fd7ea536ba48216
Juraj Smiesko <juraj.smiesko@cern.ch>
2025-06-05 15:50:25 +0200

    Calling proper analysis script

--- scripts/FCCee/IDEA/IDEA_o1_v03/IDEA_o1_v03_script.sh
+++ scripts/FCCee/IDEA/IDEA_o1_v03/IDEA_o1_v03_script.sh
@@ -11,16 +11,13 @@ echo "SETUP PHASE:"
 echo "SIM-DIGI-RECO PHASE:"
 
 echo "Starting script..."
-source $FCCCONFIG/share/FCC-config/FullSim/IDEA/IDEA_o1_v03/ctest_sim_digi_reco.sh
+source "${FCCCONFIG}/share/FCC-config/FullSim/IDEA/IDEA_o1_v03/ctest_sim_digi_reco.sh"
 
 
 # analyze simulation file
 echo "ANALYSIS PHASE:"
 
 echo "Starting analysis script..."
-python $WORKAREA/key4hep-reco-validation/scripts/FCCee/IDEA/IDEA_o1_v03/IDEA_make_TH1.py \
+python "${WORKAREA}/key4hep-reco-validation/scripts/FCCee/IDEA/IDEA_o1_v03/IDEA_make_hists.py" \
        -f IDEA_sim_digi_reco.root -o results.root
 echo "Script executed successfully"
-
-
-
