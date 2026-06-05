e57bcdde073c6eb8c1790bc028dea37f68fc12b0
Andreas Loeschcke Centeno <andreas.loeschcke.centeno@cern.ch>
2026-06-05 16:48:02 +0200

    Moved scripts to detector option+version specific directories and adapted paths accordingly

--- scripts/FCCee/ALLEGRO/ALLEGRO_o1_v03/ALLEGRO_o1_v03_script.sh
+++ scripts/FCCee/ALLEGRO/ALLEGRO_o1_v03/ALLEGRO_o1_v03_script.sh
@@ -4,20 +4,20 @@ set -e
 # setup phase
 echo "SETUP PHASE:"
 
-[ -z "$KEY4HEP_STACK" ] && source /cvmfs/sw-nightlies.hsf.org/key4hep/setup.sh
+[ -z "${KEY4HEP_STACK}" ] && source /cvmfs/sw-nightlies.hsf.org/key4hep/setup.sh
 
 
 # simulation phase
 echo "SIM-DIGI-RECO PHASE:"
 
 echo "Starting script..."
-source "${FCCCONFIG}/FullSim/IDEA/${VERSION}/ctest_sim_digi_reco.sh"
+bash "${FCCCONFIG}/FullSim/ALLEGRO/${VERSION}/ctest_sim_digi_reco.sh"
 
 
 # analyze simulation file
 echo "ANALYSIS PHASE:"
 
 echo "Starting analysis script..."
-python "${WORKAREA}/key4hep-reco-validation/scripts/FCCee/IDEA/IDEA_make_hists.py" \
-       -f IDEA_sim_digi_reco.root -o results.root
+python "${WORKAREA}/key4hep-reco-validation/scripts/FCCee/ALLEGRO/ALLEGRO_o1_v03/ALLEGRO_o1_v03_make_hists.py" \
+       -f ALLEGRO_sim_digi_reco.root -o results.root
 echo "Script executed successfully"
