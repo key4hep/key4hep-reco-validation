e1e7ee8b72fa41fb30880e347a61145c8ccbcf66
Andreas Loeschcke Centeno <53434935+s6anloes@users.noreply.github.com>
2026-06-08 11:02:34 +0300

    Allow for scripts for different detector versions again (#12)

--- scripts/FCCee/IDEA/IDEA_o1_v03/IDEA_o1_v03_script.sh
+++ scripts/FCCee/IDEA/IDEA_o1_v03/IDEA_o1_v03_script.sh
@@ -4,20 +4,20 @@ set -e
 # setup phase
 echo "SETUP PHASE:"
 
-[ -z "${KEY4HEP_STACK}" ] && source /cvmfs/sw-nightlies.hsf.org/key4hep/setup.sh
+[ -z "$KEY4HEP_STACK" ] && source /cvmfs/sw-nightlies.hsf.org/key4hep/setup.sh
 
 
 # simulation phase
 echo "SIM-DIGI-RECO PHASE:"
 
 echo "Starting script..."
-bash "${FCCCONFIG}/FullSim/ALLEGRO/${VERSION}/ctest_sim_digi_reco.sh"
+source "${FCCCONFIG}/FullSim/IDEA/${VERSION}/ctest_sim_digi_reco.sh"
 
 
 # analyze simulation file
 echo "ANALYSIS PHASE:"
 
 echo "Starting analysis script..."
-python "${WORKAREA}/key4hep-reco-validation/scripts/FCCee/ALLEGRO/ALLEGRO_make_hists.py" \
-       -f ALLEGRO_sim_digi_reco.root -o results.root
+python "${WORKAREA}/key4hep-reco-validation/scripts/FCCee/IDEA/IDEA_o1_v03/IDEA_o1_v03_make_hists.py" \
+       -f IDEA_sim_digi_reco.root -o results.root
 echo "Script executed successfully"
