bf540673d2848e2cf2d1260863b78298c790f0c8
Tim Leidel <tim.leidel@cern.ch>
2026-07-30 17:26:01 +0200

    Workaround for key4hep/key4hep-spack#901,

--- scripts/FCCee/ALLEGRO/ALLEGRO_o1_v03/ALLEGRO_o1_v03_script.sh
+++ scripts/FCCee/ALLEGRO/ALLEGRO_o1_v03/ALLEGRO_o1_v03_script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Enable strict error tracking for production/pipeline safety
-set -euo pipefail
-
 # --- Setup Environment ---
 if [ -z "${KEY4HEP_STACK:-}" ]; then
     source /cvmfs/sw-nightlies.hsf.org/key4hep/setup.sh
 fi
+
+# Enable strict error tracking for production/pipeline safety
+set -euo pipefail
 
 # Define the path to the ctest script
 CTEST_SCRIPT="${WORKAREA}/FCC-config/FCCee/FullSim/ALLEGRO/${VERSION}/ctest_sim_digi_reco.sh"
